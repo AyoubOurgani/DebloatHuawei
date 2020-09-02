9c8e32322a3a2afa2a0af004c4efdea4075bc68c
unknown <ourgani.ayoub@gmail.com>
2020-09-02 15:19:07 +0200

    first try unclean and repetitive packages

---
+++ unbloatAndroid.sh
@@ -0,0 +1,161 @@
+adb uninstall --user 0 com.google.android.ims
+
+adb uninstall --user 0 com.google.android.gms
+
+adb uninstall --user 0 com.google.android.gsf
+
+adb uninstall --user 0 com.google.android.syncadapters.contacts
+
+adb uninstall --user 0 com.google.ar.core
+
+adb uninstall --user 0 com.android.hotwordenrollment.xgoogle
+
+adb uninstall --user 0 com.android.hotwordenrollment.okgoogle
+
+adb uninstall --user 0 com.google.android.syncadapters.calendar
+
+adb uninstall --user 0 com.google.android.gms.location.history
+
+adb uninstall --user 0 com.google.android.feedback
+
+adb uninstall --user 0 com.google.android.onetimeinitializer
+
+adb uninstall --user 0 com.google.android.gms.location.history
+
+adb uninstall --user 0 com.google.android.printservice.recommendation
+
+adb uninstall --user 0 com.google.android.onetimeinitializer
+
+adb uninstall --user 0 com.google.android.apps.work.oobconfig
+
+adb uninstall --user 0 com.google.android.partnersetup
+
+adb uninstall --user 0 com.google.android.overlay.gmsconfig
+
+adb uninstall --user 0 com.google.android.ext.services
+
+adb uninstall --user 0 com.google.android.ext.shared
+
+adb uninstall --user 0 com.google.android.overlay.gmsgsaconfig
+
+adb uninstall --user 0 com.google.android.overlay.modules.permissioncontroller
+
+adb uninstall --user 0 com.google.android.overlay.settingsProvider
+
+adb uninstall --user 0 com.google.android.configupdater
+
+adb uninstall --user 0 com.google.android.setupwizard
+
+adb uninstall --user 0 com.google.android.partnersetup
+
+adb uninstall --user 0 com.google.android.setupwizard
+
+adb uninstall --user 0 com.google.android.configupdater
+
+adb uninstall --user 0 com.google.android.projection.gearhead
+
+
+
+adb uninstall --user 0 com.google.android.apps.messaging
+
+adb uninstall --user 0 com.android.vending
+
+adb uninstall --user 0 com.google.android.youtube
+
+adb uninstall --user 0 com.google.android.docs
+
+
+adb uninstall --user 0 com.google.android.tts
+
+adb uninstall --user 0 com.google.android.apps.photos
+
+adb uninstall --user 0 com.google.android.apps.maps
+
+adb uninstall --user 0 com.google.android.videos
+
+adb uninstall --user 0 com.google.android.music
+
+adb uninstall --user 0 com.google.android.gm
+
+adb uninstall --user 0 com.google.android.apps.tachyon
+
+adb uninstall --user 0 com.google.android.apps.work.oobconfig
+
+adb uninstall --user 0 com.google.android.apps.restore
+
+adb uninstall --user 0 com.android.chrome
+
+adb uninstall --user 0 com.google.android.apps.restore
+
+adb uninstall --user 0 com.google.android.gm
+
+
+
+adb uninstall --user 0 com.huawei.retaildemo
+adb uninstall --user 0 com.google.android.googlequicksearchbox
+adb uninstall --user 0 com.google.android.apps.docs.editors.sheets
+adb uninstall --user 0 com.google.android.apps.docs.editors.slides
+adb uninstall --user 0 com.google.android.apps.docs.editors.docs
+adb uninstall --user 0 com.google.android.apps.books 
+adb uninstall --user 0 com.google.android.apps.mapps
+adb uninstall --user 0 com.google.android.apps.tachyon
+adb uninstall --user 0 com.google.android.apps.cloudprint
+adb uninstall --user 0 com.google.android.apps.photos 
+adb uninstall --user 0 com.google.android.apps.docs
+adb uninstall --user 0 com.google.android.feedback
+adb uninstall --user 0 com.google.android.play.games
+adb uninstall --user 0 com.android.email
+adb uninstall --user 0 com.booking
+adb uninstall --user 0 com.amazon.mShop.android.shopping
+adb uninstall --user 0 com.android.hwmirror
+adb uninstall --user 0 com.amazon.appmanager
+adb uninstall --user 0 com.android.chrome 
+adb uninstall --user 0 com.android.bookmarkprovider
+adb uninstall --user 0 com.google.android.music
+adb uninstall --user 0 com.google.android.apps.magazines
+adb uninstall --user 0 com.google.android.marvin.talkback
+adb uninstall --user 0 com.google.android.keep
+adb uninstall --user 0 com.google.android.youtube
+adb uninstall --user 0 com.google.android.videos
+adb uninstall --user 0 com.google.android.talk
+adb uninstall --user 0 com.google.android.projection.gearhead 
+adb uninstall --user 0 com.android.partnerbrowsercustomizations.tmobile
+adb uninstall --user 0 com.baidu.input_huawei
+adb uninstall --user 0 com.android.storagemanager
+adb uninstall --user 0 com.android.providers.partnerbookmarks
+adb uninstall --user 0 com.android.partnerbrowsercustomizations
+adb uninstall --user 0 com.android.stk
+adb uninstall --user 0 com.huawei.appmarket
+adb uninstall --user 0 com.huawei.hwid
+adb uninstall --user 0 com.huawei.android.remotecontroller
+adb uninstall --user 0 com.huawei.android.chr
+adb uninstall --user 0 com.huawei.phoneservice
+adb uninstall --user 0 com.huawei.android.hwpay
+adb uninstall --user 0 com.huawei.hwapplet
+adb uninstall --user 0 com.huawei.browser
+adb uninstall --user 0 com.huawei.hifolder
+adb uninstall --user 0 com.huawei.contactscamcard
+adb uninstall --user 0 com.huawei.health
+adb uninstall --user 0 com.huawei.compass 
+adb uninstall --user 0 com.huawei.KoBackup
+adb uninstall --user 0 com.huawei.android.tips
+adb uninstall --user 0 com.android.soundrecorder
+adb uninstall --user 0 com.ironsource.appcloud.oobe.huawei
+adb uninstall --user 0 com.facebook.appmanager
+adb uninstall --user 0 com.huawei.himovie.overseas
+adb uninstall --user 0 com.huawei.wallet
+adb uninstall --user 0 com.huawei.vassistant
+adb uninstall --user 0 com.orange.update
+adb uninstall --user 0 com.facebook.katana
+adb uninstall --user 0 com.hicloud.android.clone
+adb uninstall --user 0 com.facebook.services
+adb uninstall --user 0 com.microsoft.translator
+adb uninstall --user 0 com.touchtype.swiftkey
+adb uninstall --user 0 com.huawei.tips
+adb uninstall --user 0 com.huawei.parentcontrol
+adb uninstall --user 0 com.huawei.search
+adb uninstall --user 0 com.example.android.notepad 
+adb uninstall --user 0 com.swiftkey.swiftkeyconfigurator
+adb uninstall --user 0 com.huawei.appmarket
+adb uninstall --user 0 com.android.mediacenter
+adb uninstall --user 0 com.facebook.system
